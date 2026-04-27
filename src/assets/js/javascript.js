180d368c5b10d90cde28f1d4a963cdafa5e7d3c0
mpjmar <meripeich@gmail.com>
2026-04-27 16:33:20 +0200

    Termiando CRUD y añadiendo los listeners de viajes

--- src/assets/js/javascript.js
+++ src/assets/js/javascript.js
@@ -2,7 +2,7 @@ const PORT = 3000;
 const ENDPOINT_SERVER = "http://localhost";
 const URL_BASE = `${ENDPOINT_SERVER}:${PORT}/`;
 const ENDPOINT_OBTENER_VIAJES = "viajes";
-const ENDPOINT_INSERTAR_ELIMINAR_ACTUALIZAR_VIAJES = "viaje";
+const ENDPOINT_INSERTAR_ELIMINAR_ACTUALIZAR_VIAJE = "viaje";
 
 const botonbuscartodosviajes = document.getElementById("botonbuscartodos");
 const botonbusquedaporciudad = document.getElementById("botonbusquedaporciudad");
@@ -13,7 +13,7 @@ const busquedaporpais = document.getElementById("busquedaporpais");
 const listaviajes = document.getElementById("listaviajes");
 const mensajesalida = document.getElementById("mensajesalida");
 
-function mostrarViajes(listaviajes) {
+function mostrarViajes(viajes) {
     listaviajes.innerHTML = "";
     mensajesalida.textContent = "";
 
@@ -33,7 +33,7 @@ function mostrarViajes(listaviajes) {
 }
 
 function consultarViajes(filtro = "todos", valor = "") {
-    const ENDPOINT_SERVER_VIAJES = new URL(ENDPOINT_OBTENER_VIAJES, URL_BASE);
+    const ENDPOINT_SERVER_VIAJES = new URL(ENDPOINT_OBTENER_VIAJE, URL_BASE);
 
     switch (filtro) {
         case "pais":
@@ -59,7 +59,8 @@ function consultarViajes(filtro = "todos", valor = "") {
 
 function insertarViaje(viaje) {
     const ENDPOINT_SERVER_INSERTAR_VIAJES = new 
-        URL(ENDPOINT_INSERTAR_ELIMINAR_ACTUALIZAR_VIAJES, URL_BASE);
+        URL(ENDPOINT_INSERTAR_ELIMINAR_ACTUALIZAR_VIAJE, URL_BASE);
+
     fetch(ENDPOINT_SERVER_INSERTAR_VIAJES, {
         method: "POST",
         headers: {
@@ -83,6 +84,75 @@ function insertarViaje(viaje) {
     });
 }
 
-function eliminarViaje(viaje) {
-
+function eliminarViaje(idViaje) {
+    const ENDPOINT_SERVER_ELIMINAR_VIAJES = new 
+        URL(ENDPOINT_INSERTAR_ELIMINAR_ACTUALIZAR_VIAJE + `/${idViaje}`, URL_BASE);
+    
+    fetch(ENDPOINT_SERVER_ELIMINAR_VIAJES, {
+        method: "DELETE"
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error al eliminar el viaje.");
+        }
+        return response.json();
+    })
+    .then(datos => {
+        console.log(datos);
+        alert(datos.mensaje);
+    })
+    .catch(error => {
+        console.error("Error al eliminar el viaje: ", error);
+        mensajesalida.innerHTML = `<p><b>Error</b>: ${error}</p>`;
+    });
 }
+
+function actualizarViaje(viaje) {
+    const ENDPOINT_SERVER_ACTUALIZAR_VIAJES = new
+        URL(ENDPOINT_INSERTAR_ELIMINAR_ACTUALIZAR_VIAJE + `/${viaje.idViaje}`, URL_BASE);
+    
+    fetch(ENDPOINT_SERVER_ACTUALIZAR_VIAJES, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(viaje)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error al actualizar el viaje.")
+        }
+        return response.json();
+    })
+    .then(datos => {
+        console.log(datos);
+        alert(datos.mensaje);
+    })
+    .catch(error => {
+        console.error("Error al actulizar el viaje: ", error);
+        mensajesalida.innerHTML = `<p><b>Error</b>: ${error}</p>`;
+    });
+}
+
+botonbuscartodosviajes.addEventListener("click", () => listarViajes());
+botonbusquedaporciudad.addEventListener("click", () => listarViajes("ciudad",
+    botonbusquedaporciudad.value.trim()));
+botoninsertarviaje.addEventListener("click", () => {
+    const nuevo_viaje = {
+        nomViaj: "00Prueba",
+        fechInic: "2026/07/09",
+        fechaFin: "2026/08/09"
+    };
+    insertarViaje(nuevo_viaje);
+});
+botoneliminarviaje.addEventListener("click", () => {
+    eliminarViaje(idViaje);
+});
+botonactualizarviaje.addEventListener("click", () => {
+    const viaje = {
+        nomViaj: "Maldivas",
+        fechInic: "2026/10/10",
+        fechaFin: "2026/10/20"
+    }
+    actualizarViaje(viaje);
+});
